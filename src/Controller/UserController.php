2432224ec924454a3a739b72c9d7e1f2ee6cc4cb
Joey Kaan <jkaan@users.noreply.github.com>
2017-12-19 16:51:11 +0100

    Add create user form and link to it on overview page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
 {
     /**
-     * @Route("/users")
+     * @Route("/users", name="list_users")
      */
     public function index()
     {
@@ -18,6 +23,38 @@ class UserController extends Controller
 
         return $this->render('users/index.html.twig', [
             'users' => $userRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/users/create", name="user_create")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $form = $this->createFormBuilder(new User())
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->add('create', SubmitType::class, ['label' => 'Create user'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('list_users');
+        }
+
+        return $this->render('users/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
